dc05cef8384dd75bb4126b94d2caa1e904d7445a
Kiran <kiranpadma2003@gmail.com>
2023-08-05 20:44:19 +0530

    added divider

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "./component/Intro";
+import SectionDivider from "./component/SectionDivider";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
+      <SectionDivider/>
     </main>
   );
 }
